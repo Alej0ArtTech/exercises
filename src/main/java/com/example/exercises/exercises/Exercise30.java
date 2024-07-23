986214812c653c32f86b9717e46c5782b8ac1e8d
AlejoArtTech <honki.ponki.dance@gmail.com>
2024-07-23 12:26:08 -0600

    new exercise 31

--- src/main/java/com/example/exercises/exercises/Exercise30.java
+++ src/main/java/com/example/exercises/exercises/Exercise30.java
@@ -1,19 +1,51 @@
 package com.example.exercises.exercises;
 
+import java.util.Scanner;
+
 public class Exercise30 implements Exercise {
     @Override
     public void runExercise() {
-/*         Buscar en una lista:
-        Escribe un método en Java que reciba una lista de números enteros y un número, y devuelva true si el número está en la lista y false en caso contrario.
-        
-        Ejemplo:
-        
-        Entrada: lista [5, 3, 8, 1, 2], número 3
-        
-        Salida: true
-        
-        Entrada: lista [5, 3, 8, 1, 2], número 7
-        
-        Salida: false */
+        /*
+         * Buscar en una lista:
+         * Escribe un método en Java que reciba una lista de números enteros y un
+         * número, y devuelva true si el número está en la lista y false en caso
+         * contrario.
+         * 
+         * Ejemplo:
+         * 
+         * Entrada: lista [5, 3, 8, 1, 2], número 3
+         * 
+         * Salida: true
+         * 
+         * Entrada: lista [5, 3, 8, 1, 2], número 7
+         * 
+         * Salida: false
+         */
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int numero = entrada.nextInt();
+
+        Integer[] lista = { 5, 3, 8, 1, 2 };
+
+        boolean estaContenido = false;
+
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] == numero) {
+                estaContenido= true;
+                break;
+            }
+
+        }
+
+        System.out.println(estaContenido);
+
+        /*
+         * for (int i = 0; i < lista.length; i++) {
+         * estaContenido = lista[i] == numero ? true : false;
+         * 
+         * }
+         */
+
     }
 }
--- src/main/java/com/example/exercises/exercises/Exercise31.java
+++ src/main/java/com/example/exercises/exercises/Exercise31.java
@@ -1,6 +1,11 @@
 package com.example.exercises.exercises;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Exercise31 implements Exercise {
+    private static final int ArrayList = 0;
+
     @Override
     public void runExercise() {
 
@@ -21,8 +26,42 @@ public class Exercise31 implements Exercise {
          * Salida: 13 (la secuencia es 0, 1, 1, 2, 3, 5, 8, 13, ...)
          */
 
-        
-        
-        
+        // Objetivo_ Realizar la secuencia a el numero ingresado
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese la longitud del numero fibonacci");
+        int numero = entrada.nextInt();
+        for (Integer n : lista(numero)) {
+            System.out.println(n + " ");
+
+        }
     }
+
+    public static ArrayList<Integer> lista(int numero) {
+        int nbase1 = 0;
+        int nbase2 = 1;
+        int n = 0;
+        ArrayList<Integer> listafinal = new ArrayList<>();
+        listafinal.add(nbase1);
+        listafinal.add(nbase2);
+        for (int i = 2; i < numero; i++) {
+            if (i > 2) {
+                nbase1 = nbase2;
+                nbase2 = n;
+            }
+
+            n = nbase1 + nbase2;
+            listafinal.add(n);
+            // 3 0 1 = 1
+            // 4_ 1 + 1 = 2
+            // 5_ 1 + 3 = 3
+
+            //
+            // agregar los numeros decalrados al listaa listafinal y crear la logica
+        }
+
+        return listafinal;
+    }
+
 }
