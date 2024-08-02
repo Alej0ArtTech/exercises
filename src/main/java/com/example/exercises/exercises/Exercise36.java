d0192d8c65fd30d91897539593066c2dd6c5bd5b
AlejoArtTech <honki.ponki.dance@gmail.com>
2024-08-02 15:31:32 -0600

    new exercises 35

---
+++ src/main/java/com/example/exercises/exercises/Exercise36.java
@@ -0,0 +1,12 @@
+package com.example.exercises.exercises;
+
+public class Exercise36 implements Exercise {
+    @Override
+    public void runExercise() {
+
+/*         Write a Java program that then reads an integer and calculates the sum of its digits
+and writes the number of each digit of the sum in English */
+
+        
+    }
+}
