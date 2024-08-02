d220818a5ea3a8df36a4b8687a8ab2e6d961cee7
AlejoArtTech <honki.ponki.dance@gmail.com>
2024-08-02 15:31:58 -0600

    exercise 27

---
+++ src/main/java/com/example/exercises/exercises/Exercise37.java
@@ -0,0 +1,8 @@
+package com.example.exercises.exercises;
+
+public class Exercise37 implements Exercise {
+    @Override
+    public void runExercise() {
+        // TODO: Implement the exercise logic here
+    }
+}
---
+++ src/main/java/com/example/exercises/exercises/Exercise38.java
@@ -0,0 +1,8 @@
+package com.example.exercises.exercises;
+
+public class Exercise38 implements Exercise {
+    @Override
+    public void runExercise() {
+        // TODO: Implement the exercise logic here
+    }
+}
