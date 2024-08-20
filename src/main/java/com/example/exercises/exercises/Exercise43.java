aa0d60a3783709aa42a9e4b21e5ae3ecc78691a3
AlejoArtTech <honki.ponki.dance@gmail.com>
2024-08-20 08:28:43 -0600

    Second commit

---
+++ src/main/java/com/example/exercises/exercises/Exercise43.java
@@ -0,0 +1,9 @@
+package com.example.exercises.exercises;
+
+public class Exercise43 implements Exercise {
+    @Override
+    public void runExercise() {
+        // TODO: Implement the exercise logic here
+        //teams.microsoft.com/l/meetup-join/19%3ameeting_NTc4NjRiMTUtMzY2MC00NDJhLTk0ZDYtMzk4ZDA5ZTQyYjNj%40thread.v2/0?context=%7b%22Tid%22%3a%22864ee999-c538-44a0-bddd-0449de62df6d%22%2c%22Oid%22%3a%2292e3beca-217e-40d2-a5b9-54a736c045b3%22%7d5441
+    }
+}
